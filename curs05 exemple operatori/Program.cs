943513fc754809a4e51b48f48981402c0951cadc
Iosif Turcu <iosif.turcu@emmanuel.ro>
2021-10-29 10:18:45 +0300

    Initial project setup.

---
+++ curs05 exemple operatori/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace curs05_exemple_operatori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
